12a454c1226a103dbeed72ab58d065e850803ddc
fel1xs <140620040+fel1xs@users.noreply.github.com>
2023-07-28 19:39:51 +0200

    fixed time windows, better standby,code cleaned up

--- log_info.php
+++ log_info.php
@@ -4,23 +4,30 @@ function logPlayerInfo() {
     $name = $requestData['name'];
     $ipAddress = $_SERVER['REMOTE_ADDR'];
     $currentTimeStamp = date("Y-m-d H:i:s");
-    $logText = "$name:\nlast-ip: $ipAddress\nlast-online: $currentTimeStamp\n";
+    $logText = "$name:\nlast-ip: $ipAddress\nlast-online: $currentTimeStamp";
 
     // Read the current content of log.txt
     $logFile = 'log.txt';
-    $currentLogContent = file_get_contents($logFile);
+    $currentLogContent = trim(file_get_contents($logFile));
 
     // Check if an entry with the given name already exists
     $namePattern = "/^$name:\nlast-ip: (.*)\nlast-online: (.*)$/m";
     if (preg_match($namePattern, $currentLogContent, $matches)) {
-        // If the entry exists, update the IP address and timestamp
+        // If the entry exists, update only the specific entry with the given name
         $oldIPAddress = $matches[1];
         $oldTimeStamp = $matches[2];
-        $updatedLogText = str_replace("last-ip: $oldIPAddress", "last-ip: $ipAddress", $currentLogContent);
-        $updatedLogText = str_replace("last-online: $oldTimeStamp", "last-online: $currentTimeStamp", $updatedLogText);
+        $updatedLogText = preg_replace(
+            "/^$name:\nlast-ip: $oldIPAddress\nlast-online: $oldTimeStamp$/m",
+            "$logText",
+            $currentLogContent,
+            1
+        );
         file_put_contents($logFile, $updatedLogText);
     } else {
         // If the entry does not exist, add a new entry
+        if ($currentLogContent !== '') {
+            $logText = PHP_EOL . $logText;
+        }
         file_put_contents($logFile, $logText . PHP_EOL, FILE_APPEND);
     }
 }
--- update_settings.php
+++ update_settings.php
@@ -1,4 +1,8 @@
 <?php
+// Set headers to prevent caching
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0"); // Proxies.
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $requestData = json_decode(file_get_contents('php://input'), true);
     $settings = array(
